40b23fd56afbe155000092c5f06354e9ad6cf049
G.M <g.mjardev@gmail.com>
2022-11-03 13:05:00 -0500

    Overview: Finished the putAt method, will need to redesign it since nodes need to be rewritten. Details: 1. Finished iterative putAt method instead of recursive one. 2. Finished get method utilizing the ordering. TODO: 1. Redesign nodes so resizing adding can be performed while resizing.

--- Maps/ChainMap/ChainMap.go
+++ Maps/ChainMap/ChainMap.go
@@ -2,46 +2,118 @@ package ChainMap
 
 import (
 	"GMUtils/Maps"
-	"GMUtils/Maps/ArrMap"
+	"hash/maphash"
 	"sync/atomic"
-	"unsafe"
 )
 
 type ChainMap[K Maps.Hashable, V any] struct {
-	buckets []ArrMap.head[K, V]
-	chunk   uint8
+	buckets []head[K, V]
+	chunk   byte
+	seed    maphash.Seed
 }
 
-func (u *ChainMap[K, V]) searchHash(h *ArrMap.head[K, V], hash uint64) *ArrMap.chain[K, V] {
-	var pre *ArrMap.chain[K, V] = nil
-	cur := h.next()
-	for ; cur != nil && uint64(cur.k.Hash()) < hash; cur = cur.next() {
-		pre = cur
-	}
-	return pre
+func MakeChainMap[K Maps.Hashable, V any](size uint) *ChainMap[K, V] {
+	t := new(ChainMap[K, V])
+	t.buckets = make([]head[K, V], size)
+	t.seed = maphash.MakeSeed()
+	t.chunk = 64
+	return t
 }
 
-func (u *ChainMap[K, V]) putAt(h *ArrMap.head[K, V], k K, v V) {
-begin:
-	var preh *ArrMap.head[K, V] = h
-	cur := h.next()
-	for ; cur != nil && uint64(cur.k.Hash()) <= uint64(k.Hash()) && !k.Equal(cur.k); cur = cur.next() {
-		preh = &cur.head
+func (u ChainMap[K, V]) rehash(k K) uint64 {
+	//b := make([]byte, 8)
+	//binary.PutVarint(b, k.Hash())
+	//return maphash.Bytes(u.seed, b)
+	return uint64(k.Hash())
+}
+
+// [0,2^n) to [0,2^n-1),[2^n-1,2^n)
+func (u ChainMap[K, V]) splitBucket() {
+	newBuckets := make([]head[K, V], len(u.buckets)*2)
+	newChunk := u.chunk - 1
+	for i, b := range u.buckets {
+
 	}
-	if cur != nil && cur.k.Equal(k) {
-		cur.v = v
-		return
-	} else {
-		added := false
-		for !added {
-			newCurPtr := preh.nextPtr()
-			if newCurPtr == nil || uint64(k.Hash()) <= uint64((*ArrMap.chain[K, V])(newCurPtr).k.Hash()) {
-				t := ArrMap.chain[K, V]{ArrMap.head[K, V]{newCurPtr}, k, v, false}
-				added = atomic.CompareAndSwapPointer(&preh.nx, newCurPtr, unsafe.Pointer(&t))
+}
+
+func (u *ChainMap[K, V]) putAt1(h *head[K, V], info *Hold[K, V]) {
+	for pre, curPtr := h, h.nextPtr(); ; curPtr = pre.nextPtr() {
+		if curPtr == nil {
+			if atomic.CompareAndSwapPointer(&pre.nx, curPtr, info.makeNode(curPtr)) {
+				break
+			}
+		} else {
+			cur := (*chain[K, V])(curPtr)
+			if info.isKey(cur.k) {
+				cur.v = info.val
+				break
+			} else if info.hash <= u.rehash(cur.k) {
+				if atomic.CompareAndSwapPointer(&pre.nx, curPtr, info.makeNode(curPtr)) {
+					break
+				}
+			} else if info.hash > u.rehash(cur.k) {
+				pre = &cur.head
 			} else {
-				goto begin
+				panic("unexpected case")
 			}
 		}
-
 	}
+
+}
+
+//func (u *ChainMap[K, V]) putAt2(prev *head[K, V], info *Hold[K, V]) bool {
+//	for {
+//		curPtr := prev.nextPtr()
+//		cur := (*chain[K, V])(curPtr)
+//		if curPtr == nil {
+//			if atomic.CompareAndSwapPointer(&prev.nx, curPtr, info.makeNode(curPtr)) {
+//				break
+//			}
+//		} else if info.isKey(cur.k) {
+//			cur.v = info.val
+//			break
+//		} else if info.hash <= u.rehash(cur.k) {
+//			if atomic.CompareAndSwapPointer(&prev.nx, curPtr, info.makeNode(curPtr)) {
+//				break
+//			}
+//		} else if info.hash > u.rehash(cur.k) {
+//			if u.putAt2(&cur.head, info) {
+//				break
+//			}
+//		} else {
+//			panic("unexpected case")
+//		}
+//	}
+//
+//	return true
+//}
+
+func (u ChainMap[K, V]) PrintAll() {
+	for cur := u.buckets[0].next(); cur != nil; cur = cur.next() {
+		println("k: ", cur.k, ". v: ", cur.v)
+	}
+}
+
+func (u *ChainMap[K, V]) Put(key K, val V) {
+	bucket := &u.buckets[u.rehash(key)>>u.chunk]
+	u.putAt1(bucket, &Hold[K, V]{key, val, u.rehash(key)})
+}
+
+func (u *ChainMap[K, V]) Remove(key K) {
+	for cur := u.buckets[u.rehash(key)>>u.chunk].next(); cur != nil && u.rehash(cur.k) <= u.rehash(key); cur = cur.next() {
+		if cur.k.Equal(key) {
+			cur.delete()
+			break
+		}
+	}
+}
+
+func (u ChainMap[K, V]) Get(key K) (r V) {
+	for cur := u.buckets[u.rehash(key)>>u.chunk].next(); cur != nil && u.rehash(cur.k) <= u.rehash(key); cur = cur.next() {
+		if cur.k.Equal(key) {
+			r = cur.v
+			break
+		}
+	}
+	return
 }
--- Maps/ChainMap/Nodes.go
+++ Maps/ChainMap/Nodes.go
@@ -2,11 +2,49 @@ package ChainMap
 
 import (
 	"GMUtils/Maps"
+	"sync/atomic"
 	"unsafe"
 )
 
-type hold[K Maps.Hashable, V any] interface {
-	next() hold[K, V]
-	nextPtr() unsafe.Pointer
-	isHead() bool
+type head[K Maps.Hashable, V any] struct {
+	nx unsafe.Pointer
+}
+
+type chain[K Maps.Hashable, V any] struct {
+	head[K, V]
+	k   K
+	v   V
+	del uint32
+}
+
+func (u *head[K, V]) next() *chain[K, V] {
+	return (*chain[K, V])(u.nextPtr())
+}
+
+func (u *head[K, V]) nextPtr() unsafe.Pointer {
+	for oldNext := atomic.LoadPointer(&u.nx); oldNext != nil; oldNext = atomic.LoadPointer(&u.nx) { //find the next node if there exists one
+		if t := (*chain[K, V])(oldNext); atomic.LoadUint32(&t.del) != 0 {
+			atomic.CompareAndSwapPointer(&u.nx, oldNext, atomic.LoadPointer(&t.nx)) //current node is marked, try to delete it
+		} else {
+			return oldNext
+		}
+	}
+	return nil
+}
+
+func (u *chain[K, V]) delete() {
+	atomic.StoreUint32(&u.del, 1)
+}
+
+type Hold[K Maps.Hashable, V any] struct {
+	key  K
+	val  V
+	hash uint64
+}
+
+func (u Hold[K, V]) makeNode(nx unsafe.Pointer) unsafe.Pointer {
+	return unsafe.Pointer(&chain[K, V]{head[K, V]{nx}, u.key, u.val, 0})
+}
+func (u Hold[K, V]) isKey(k Maps.Hashable) bool {
+	return u.key.Equal(k)
 }
