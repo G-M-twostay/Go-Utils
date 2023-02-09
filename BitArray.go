2f1ccfb9084163278725722a0b720db1e0d61271
G.M <g.mjardev@gmail.com>
2023-02-08 21:18:52 -0600

    Overview: Optimized BitArray.go. Details: 1. Used bit wise operation instead of division and modulo. TODO:

--- BitArray.go
+++ BitArray.go
@@ -4,8 +4,10 @@ import (
 	"math/bits"
 )
 
+const LOG_UINT_SIZE int = 5 + (bits.UintSize >> 6) //2^5==32 or 2^6==64
+
 func New(size int) BitArray {
-	return BitArray{bits: make([]uint, size/bits.UintSize)}
+	return BitArray{bits: make([]uint, 1+(size>>LOG_UINT_SIZE))}
 }
 
 type BitArray struct {
@@ -13,17 +15,18 @@ type BitArray struct {
 }
 
 func (u BitArray) Len() int {
-	return len(u.bits) * bits.UintSize
+	return len(u.bits) << LOG_UINT_SIZE
 }
 
 func (u BitArray) Get(i int) bool {
-	return (u.bits[i/bits.UintSize]>>(i%bits.UintSize))&1 == 1
+	t := 1 << i & (bits.UintSize - 1)
+	return u.bits[i>>LOG_UINT_SIZE]&t == t
 }
 
 func (u BitArray) Up(i int) {
-	u.bits[i/bits.UintSize] |= 1 << (i % bits.UintSize)
+	u.bits[i>>LOG_UINT_SIZE] |= 1 << (i & (bits.UintSize - 1))
 }
 
 func (u BitArray) Down(i int) {
-	u.bits[i/bits.UintSize] &^= 1 << (i % bits.UintSize)
+	u.bits[i>>LOG_UINT_SIZE] &^= 1 << (i & (bits.UintSize - 1))
 }
