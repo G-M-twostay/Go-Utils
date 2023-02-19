71d49eab2b6ee97cffc197a2c3fbc27d5701cd2a
G.M <g.mjardev@gmail.com>
2023-02-19 14:01:40 -0600

    Overview: Improved BitArray.go. Details: 1. Used type def instead of struct. TODO: 1. Finish HashSet.go.

--- BitArray.go
+++ BitArray.go
@@ -8,48 +8,57 @@ const LogUintSize int = 5 + (bits.UintSize >> 6) //2^5==32 or 2^6==64
 
 func NewBitArray(size uint) BitArray {
 	if size&(bits.UintSize-1) == 0 {
-		return BitArray{bits: make([]uint, size>>LogUintSize)}
+		return make([]uint, size>>LogUintSize)
 	}
-	return BitArray{bits: make([]uint, 1+(size>>LogUintSize))}
+	return make([]uint, 1+(size>>LogUintSize))
 }
 
-type BitArray struct {
-	bits []uint
-}
+type BitArray []uint
 
 func (u BitArray) Len() int {
-	return len(u.bits) << LogUintSize
+	return len(u) << LogUintSize
 }
 
 func (u BitArray) Get(i int) bool {
 	//t := uint(1 << (i & (bits.UintSize - 1)))
 	//or X&t==t
-	return u.bits[i>>LogUintSize]>>(i&(bits.UintSize-1))&1 == 1
+	return u[i>>LogUintSize]>>(i&(bits.UintSize-1))&1 == 1
 }
 
 func (u BitArray) Set(i int) {
-	u.bits[i>>LogUintSize] |= 1 << (i & (bits.UintSize - 1))
+	u[i>>LogUintSize] |= 1 << (i & (bits.UintSize - 1))
 }
 
 func (u BitArray) Clr(i int) {
-	u.bits[i>>LogUintSize] &^= 1 << (i & (bits.UintSize - 1))
+	u[i>>LogUintSize] &^= 1 << (i & (bits.UintSize - 1))
 }
 
 func (u BitArray) Invert(i int) bool {
 	t := u.Get(i)
-	u.bits[i>>LogUintSize] ^= 1 << (i & (bits.UintSize - 1))
+	u[i>>LogUintSize] ^= 1 << (i & (bits.UintSize - 1))
 	return t
 }
 
-func (u BitArray) Uints() []uint {
-	return u.bits
-}
-
 func (u BitArray) First() int {
-	for i, c := range u.bits {
+	for i, c := range u {
 		if c > 0 {
 			return i<<LogUintSize + bits.LeadingZeros(c)
 		}
 	}
 	return -1
+}
+
+func (u BitArray) Empty() bool {
+	for _, c := range u {
+		if c != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (u BitArray) Append(unitCount uint) BitArray {
+	newArr := make([]uint, uint(len(u))+unitCount)
+	copy(newArr, u)
+	return newArr
 }
