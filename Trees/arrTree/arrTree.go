83952a123eb215f4ef445dbf045f5947e9b809b5
G.M <g.mjardev@gmail.com>
2024-06-23 20:30:32 +0800

    1. progress on array based implementation.

---
+++ Trees/arrTree/arrTree.go
@@ -0,0 +1,163 @@
+package Trees
+
+import (
+	"cmp"
+	"golang.org/x/exp/constraints"
+)
+
+type SBTree[T cmp.Ordered, S constraints.Unsigned] struct {
+	base[T, S]
+}
+
+func New[T cmp.Ordered, S constraints.Unsigned](hint S) *SBTree[T, S] {
+	if hint < 1 {
+		hint = 1
+	}
+	return &SBTree[T, S]{base[T, S]{ifs: make([]info[S], hint), vs: make([]T, hint)}}
+}
+
+//func Build[T cmp.Ordered, S constraints.Unsigned](sli []T) *SBTree[T, S] {
+//	z := new(node[T, S])
+//	z.l, z.r = z, z
+//	var build func([]T) nodePtr[T, S]
+//	build = func(s []T) nodePtr[T, S] {
+//		if len(s) > 0 {
+//			mid := len(s) >> 1
+//			return &node[T, S]{s[mid], S(len(s)), build(s[0:mid]), build(s[mid+1:])}
+//		} else {
+//			return z
+//		}
+//	}
+//	return &SBTree[T, S]{base[T, S]{build(sli), z}}
+//}
+
+// target value stored in v[0]
+func (u *SBTree[T, S]) insert(curI *S) bool {
+	if *curI == 0 {
+		if u.free == 0 {
+			*curI = S(len(u.ifs))
+			u.ifs = append(u.ifs, info[S]{0, 0, 1})
+			u.vs = append(u.vs, u.vs[0])
+		} else {
+			*curI = u.popFree()
+			u.ifs[*curI], u.vs[*curI] = info[S]{0, 0, 1}, u.vs[0]
+		}
+		return true
+	} else {
+		cur := &u.ifs[*curI]
+		inserted := false
+		if u.vs[0] < u.vs[*curI] {
+			inserted = u.insert(&cur.l)
+		} else if u.vs[0] == u.vs[*curI] {
+			return false
+		} else {
+			inserted = u.insert(&cur.r)
+		}
+		if inserted {
+			cur.sz++
+			//u.maintain(curPtr, v >= cur.v)
+			if u.vs[0] >= u.vs[*curI] {
+				u.maintainRight(curI)
+			} else {
+				u.maintainLeft(curI)
+			}
+		}
+		return inserted
+	}
+
+}
+
+func (u *SBTree[T, S]) Insert(v T) bool {
+	u.vs[0] = v
+	return u.insert(&u.root)
+}
+
+// vs[0] is target value
+func (u *SBTree[T, S]) remove(curI *S) (deleted byte) {
+	if *curI == 0 {
+		return
+	} else {
+		cur := &u.ifs[*curI]
+		if u.vs[0] < u.vs[*curI] {
+			deleted = u.remove(&cur.l)
+		} else if u.vs[0] == u.vs[*curI] {
+			deleted = 1
+			if cur.l == 0 {
+				u.addFree(*curI)
+				*curI = cur.r
+			} else if cur.r == 0 {
+				u.addFree(*curI)
+				*curI = cur.l
+			} else {
+				ni := &cur.r
+				for ; u.ifs[*ni].l != 0; ni = &u.ifs[*ni].l {
+					u.ifs[*ni].sz--
+				}
+				u.vs[*curI] = u.vs[*ni]
+				u.addFree(*ni)
+				*ni = 0
+			}
+		} else {
+			deleted = u.remove(&cur.r)
+		}
+		cur.sz -= S(deleted)
+		return
+	}
+}
+
+func (u *SBTree[T, S]) Remove(v T) bool {
+	u.vs[0] = v
+	return u.remove(&u.root) != 0
+}
+
+func (u *SBTree[T, S]) Has(v T) bool {
+	for curI := u.root; curI != 0; {
+		if v < u.vs[curI] {
+			curI = u.ifs[curI].l
+		} else if v == u.vs[curI] {
+			return true
+		} else {
+			curI = u.ifs[curI].r
+		}
+	}
+	return false
+}
+
+func (u *SBTree[T, S]) Predecessor(v T) (p *T) {
+	for curI := u.root; curI != 0; {
+		if v <= u.vs[curI] {
+			curI = u.ifs[curI].l
+		} else {
+			p = &u.vs[curI]
+			curI = u.ifs[curI].r
+		}
+	}
+	return
+}
+
+func (u *SBTree[T, S]) Successor(v T) (p *T) {
+	for curI := u.root; curI != 0; {
+		if v < u.vs[curI] {
+			p = &u.vs[curI]
+			curI = u.ifs[curI].l
+		} else {
+			curI = u.ifs[curI].r
+		}
+	}
+	return
+}
+
+func (u *SBTree[T, S]) RankOf(v T) S {
+	var ra S = 0
+	for curI := u.root; curI != 0; {
+		if lci := u.ifs[curI].l; v < u.vs[curI] {
+			curI = lci
+		} else if v == u.vs[curI] {
+			return ra + u.ifs[lci].sz + 1
+		} else {
+			ra += u.ifs[lci].sz + 1
+			curI = u.ifs[curI].r
+		}
+	}
+	return 0
+}
---
+++ Trees/arrTree/arrbase.go
@@ -0,0 +1,129 @@
+package Trees
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+// A node in the SBTree
+// The zero value is meaningless.
+type info[T constraints.Unsigned] struct {
+	l, r, sz T
+}
+
+func (u *base[T, S]) rotateLeft(ni *S) {
+	n := &u.ifs[*ni]
+	rci := n.r
+
+	n.r = u.ifs[rci].l
+
+	u.ifs[rci].l, u.ifs[rci].sz = *ni, n.sz
+
+	n.sz = u.ifs[n.l].sz + u.ifs[n.r].sz + 1
+
+	*ni = rci
+}
+
+func (u *base[T, S]) rotateRight(ni *S) {
+	n := &u.ifs[*ni]
+	lci := n.l
+
+	n.l = u.ifs[lci].r
+
+	u.ifs[lci].r, u.ifs[lci].sz = *ni, n.sz
+
+	n.sz = u.ifs[n.l].sz + u.ifs[n.r].sz + 1
+
+	*ni = lci
+}
+
+type base[T any, S constraints.Unsigned] struct {
+	root, free S         //free is the beginning of the linked list that contains all the free indexes, in which case we use l as next.
+	ifs        []info[S] //0 is loopback nil
+	vs         []T       //vs[0] is temporary store for recursion
+}
+
+func (u *base[T, S]) Size() uint {
+	return uint(u.ifs[u.root].sz)
+}
+
+// adds a free index
+func (u *base[T, S]) addFree(a S) {
+	u.ifs[a].l = u.free
+	u.free = a
+}
+
+// gets a free index
+func (u *base[T, S]) popFree() S {
+	b := u.free
+	u.free = u.ifs[u.free].l
+	return b
+}
+func (u *base[T, S]) maintainLeft(curI *S) {
+	cur := u.ifs[*curI]
+	if rc, lc := u.ifs[cur.r], u.ifs[cur.l]; u.ifs[lc.l].sz > rc.sz {
+		u.rotateRight(curI)
+	} else if u.ifs[lc.r].sz > rc.sz {
+		u.rotateLeft(&cur.l)
+		u.rotateRight(curI)
+	} else {
+		return
+	}
+	u.maintainLeft(&cur.l)
+	u.maintainRight(&cur.r)
+	u.maintainLeft(curI)
+	u.maintainRight(curI)
+}
+
+func (u *base[T, S]) maintainRight(curI *S) {
+	cur := u.ifs[*curI]
+	if rc, lc := u.ifs[cur.r], u.ifs[cur.l]; u.ifs[rc.r].sz > lc.sz {
+		u.rotateLeft(curI)
+	} else if u.ifs[rc.l].sz > lc.sz {
+		u.rotateRight(&cur.r)
+		u.rotateLeft(curI)
+	} else {
+		return
+	}
+	u.maintainLeft(&cur.l)
+	u.maintainRight(&cur.r)
+	u.maintainLeft(curI)
+	u.maintainRight(curI)
+}
+
+//	func (u *base[T, S]) maintain(curI S, rightBigger bool) {
+//		if rightBigger {
+//			u.maintainRight(curI)
+//		} else {
+//			u.maintainLeft(curI)
+//		}
+//	}
+func (u *base[T, S]) inOrder(curI S, f func(v T) bool) {
+	cur := u.ifs[curI]
+	if cur.l != 0 {
+		u.inOrder(cur.l, f)
+	}
+	if curI != 0 {
+		if !f(u.vs[curI]) {
+			return
+		}
+	}
+	if cur.r != 0 {
+		u.inOrder(cur.r, f)
+	}
+}
+func (u *base[T, S]) InOrder(f func(v T) bool) {
+	u.inOrder(u.root, f)
+}
+func (u *base[T, S]) KLargest(k S) *T {
+	for curI := u.root; curI != 0; {
+		if lc := u.ifs[u.ifs[curI].l]; k <= lc.sz {
+			curI = lc.l
+		} else if k == lc.sz+1 {
+			return &u.vs[curI]
+		} else {
+			k -= lc.sz + 1
+			curI = u.ifs[curI].r
+		}
+	}
+	return nil
+}
